cec8c7df3adcdcd3642e45e1b9a8be0e1b41e28e
Aiden Kenny <akenny430@gmail.com>
2023-06-14 23:42:06 -0400

    added data with null values

--- data/make_gaussian.py
+++ data/make_gaussian.py
@@ -27,3 +27,25 @@ for i in range(1, 11):
         )
     )
     df.write_csv(f"./data/{i:02}.csv") 
+
+# now making some where there are null values 
+for i in range(11, 16): 
+    gaussian_data: np.ndarray = _RNG.normal(loc=0.0 + i*0.25, scale=1.0 + i*0.05, size=len(_DATES))
+    which_to_set_nan: np.ndarray = _RNG.choice(a=len(_DATES), size=250, replace=False)
+    gaussian_data[which_to_set_nan] = np.nan 
+    df: pl.DataFrame = (
+        pl.DataFrame(
+            data={
+                "Date": _DATES, 
+                "X": gaussian_data
+            }, 
+        )
+        .with_columns([
+            pl.col("Date").cast(pl.Date).keep_name(), 
+            pl.when(pl.col("X").is_nan())
+            .then(None)
+            .otherwise(pl.col("X"))
+            .keep_name() 
+        ])
+    )
+    df.write_csv(f"./data/{i:02}.csv") 
