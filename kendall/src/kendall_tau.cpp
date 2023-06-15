f417887ff920b1bfa3a483a77f6edeaaea3e2099
Aiden Kenny <akenny430@gmail.com>
2023-06-15 05:08:20 -0400

    initial permutation of y

--- kendall/src/kendall_tau.cpp
+++ kendall/src/kendall_tau.cpp
@@ -9,10 +9,58 @@
 
 
 
+auto _get_permutation_index(const std::vector<double>& vals) -> std::vector<std::size_t>
+{
+    std::vector<std::size_t> index ( vals.size(), 0 );
+    for (std::size_t i { 0 }; i < index.size() ; ++i) {
+        index[i] = i;
+    }
+    std::sort(
+        index.begin(), 
+        index.end(), 
+        [&](const int& a, const int& b) {
+            return (vals[a] < vals[b]); 
+        }
+    ); 
+    return index; 
+}
+
+
 auto _compute_kendall_tau_with_full(const std::vector<double>& x, const std::vector<double>& y) -> float
 {
-    int size { x.size() }; 
+    int size { static_cast<int>(x.size()) }; 
     int v_total { size * (size - 1) / 2 }; 
+
+    // first permutations of y 
+    std::vector<std::size_t> y_sort_indices = _get_permutation_index(y); 
+
+    std::vector<int> y_cum_sum {  }; 
+    int current_cum_sum { 1 }; // always starts with 1 
+    int times_seen { 1 }; 
+    // for( const std::size_t index : y_sort_indices ) 
+    std::size_t _prev_val { static_cast<std::size_t>( y.at( y_sort_indices.at(0) ) ) }; 
+    for( std::size_t _i { 1 }; _i < y_sort_indices.size(); ++_i ) 
+    {
+        if( y.at( y_sort_indices.at(_i) ) == _prev_val )
+        {
+            ++times_seen; 
+            continue; 
+        }
+        while( times_seen > 0 )
+        {
+            y_cum_sum.push_back( current_cum_sum ); 
+            --times_seen; 
+        }
+        times_seen = 1; 
+        ++current_cum_sum; 
+        _prev_val = y.at( y_sort_indices.at(_i) ); 
+    }
+
+    // test printing 
+    for( int j = 0; j < 20; ++j )
+    {
+        std::cout << "(" << y[y_sort_indices[j]] << ", " << y_cum_sum[j] << ")\n"; 
+    }
 
 
     return 0.0; 
